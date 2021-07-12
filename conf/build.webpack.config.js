ac9cdc7d2df5c0e655631cd70faed351fb429f28
M4x1m3 <M4x1me@protonmail.com>
2021-07-12 17:44:24 +0200

    Split webpack config, put it in conf

--- conf/build.webpack.config.js
+++ conf/build.webpack.config.js
@@ -1,12 +1,7 @@
 const path = require('path');
 module.exports = {
   // the enter path
-  entry: "./example/index.js",
-  devServer: {
-    contentBase: './example/',
-    port: 3000,
-    index: 'index.html'
-  },
+  entry: "./index.js",
   output: {
     path: path.resolve(__dirname, '../dist/'),
     filename: 'numworks.js',
