b852de79db8685b14721d43a8898fc66a741510b
M4x1m3 <M4x1me@protonmail.com>
2021-07-12 14:15:11 +0200

    Added build command

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
     contentBase: './example/',
     port: 3000,
     index: 'index.html'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist/'),
+    filename: 'numworks.js',
+    library: 'Numworks'
   }
 }
 
